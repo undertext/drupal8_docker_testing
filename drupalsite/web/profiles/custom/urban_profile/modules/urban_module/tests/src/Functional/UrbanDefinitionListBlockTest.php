62b937bdf01f0bd890ecc90024ccd456a9562f94
Yoroslav Kharchenko <yaroslav.kharchenko@mediamonks.net>
2019-05-07 15:16:34 +0200

    Adding Functional test.

---
+++ drupalsite/web/profiles/custom/urban_profile/modules/urban_module/tests/src/Functional/UrbanDefinitionListBlockTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\tests\urban_module\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests urban definition list block.
+ */
+class UrbanDefinitionListBlockTest extends BrowserTestBase {
+
+  protected $profile = 'urban_profile';
+
+  /**
+   * Tests that block is correctly displayed on the frontpage.
+   */
+  public function testBlock() {
+    $this->createNode([
+      'type' => 'urban_definition',
+      'title' => 'brb',
+    ]);
+    $this->createNode([
+      'type' => 'urban_definition',
+      'title' => 'lmk',
+    ]);
+
+    $this->drupalGet('<front>');
+
+    $this->assertSession()->pageTextContains('brb');
+    $this->assertSession()->pageTextContains('lmk');
+  }
+
+}
